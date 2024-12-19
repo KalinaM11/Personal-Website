a63a75f35053a7db0da02810107d09427875acb1
KalinaM11 <kalina.monova@gmail.com>
2024-12-19 01:43:33 -0500

    initial commits

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'light-pink' : 'FCE8F3',
+      'beige' : 'E5D9CC',
+      'my-pink' : 'D7BDCB'
+    }
+  },
+  plugins: [],
+}
+
